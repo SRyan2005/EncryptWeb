d8a8127a9b66ad3b90c309cf2d1a3d426f0d4859
Flame48 <advait.s.rajan@gmail.com>
2022-09-11 11:48:22 +0400

    Added Pagination to Archive

--- JS/fetchArchLinks.js
+++ JS/fetchArchLinks.js
@@ -1,5 +1,10 @@
-let videosUl = document.getElementById('videos');
+let pagesUl = document.getElementById('pages');
+let pagesNav = document.getElementById('pageNav');
 let videoList = [];
+let pages = [];
+let currentPageIndx = 0;
+let numOfPages;
+let vidPerPage = 10;
 let channelId = 'UCW3u7kksReiKE5vgPLDNznQ';
 
 //The below http request fetches the youtube playlist data for the channels videos in json format.
@@ -10,8 +15,15 @@ const url = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2C
 let fetched = false;
 fetch(url)
 .then(data=>{return data.json()})
-.then(res=>{storeVideoData(res.items)})
-.then(rend=>{renderVideos()})
+.then(res=>{main(res.items)})
+
+function main(inpData) {
+    storeVideoData(inpData);
+    pagination();
+    console.log(pages);
+    renderVideos();
+    addPageLinks();
+}
 
 function storeVideoData(inpData) {
     videoList = [];
@@ -27,20 +39,66 @@ function storeVideoData(inpData) {
 }
 
 function renderVideos() {
-    let html = ``;
-    for(let vid of videoList) {
-        let tmp =
-        `
-        <li>
-            <span class="videoContainer">
-                <iframe class="video"
-                src="https://www.youtube.com/embed/` + vid.id + `" frameborder="0"
-                allowfullscreen="true" loading="lazy"></iframe>
-            </span>
-            <a href="https://www.youtube.com/watch?v=` + vid.id + `">` + vid.title + `</a>
+    let finalHtml = ``;
+    let cnt = 0;
+    for(let page of pages) {
+        let pageHtml = `
+        <li class="`+((cnt===0)?`active`:`inactive`)+`" id="pageNo`+cnt+`">
+            <ul class="videoList">
+        `;
+        cnt++;
+        for(let v of page) {
+            pageHtml += `
+            <li>
+                <span class="videoContainer">
+                    <iframe class="video"
+                    src="https://www.youtube.com/embed/` + v.id + `" frameborder="0"
+                    allowfullscreen="true" loading="lazy"></iframe>
+                </span>
+                <a href="https://www.youtube.com/watch?v=` + v.id + `">` + v.title + `</a>
+            </li>
+            `;
+        }
+        pageHtml += `
+            </ul>
         </li>
         `;
-        html += tmp;
+        finalHtml += pageHtml;
     }
-    videosUl.innerHTML = html;
+    pagesUl.innerHTML = finalHtml;
+}
+
+function addPageLinks() {
+    let finalHtml = `
+    `;
+    for(let i=0; i<pages.length; i++) {
+        finalHtml+=`
+        <a onclick="switchPage(`+i+`)" class="`+((i==currentPageIndx)?`current`:``)+`">`+i+`</a>
+        `;
+    }
+    pagesNav.innerHTML = finalHtml;
+}
+
+function pagination() {
+    let pageCnt = 0;
+    pages = [[]];
+    for(let i=0; i<videoList.length; i++) {
+        if(pages[pageCnt].length>=vidPerPage) {
+            pageCnt++;
+            pages.push([]);
+        }
+        pages[pageCnt].push(videoList[i]);
+    }
+    numOfPages = pages.length;
+}
+
+function switchPage(indx) {
+    let currentPage = document.getElementById('pageNo'+currentPageIndx);
+    currentPage.classList.remove('active');
+    currentPage.classList.add('inactive');
+    currentPageIndx = indx;
+    let nextPage = document.getElementById('pageNo'+indx);
+    nextPage.classList.remove('inactive');
+    nextPage.classList.add('active');
+    addPageLinks();
 }
